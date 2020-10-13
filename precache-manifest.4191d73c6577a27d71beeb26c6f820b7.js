1b6136c08c113ab628ef848ceee306c39be9d11b
langdinggai <1845255483@qq.com>
2020-10-13 15:49:46 +0800

    Updates

---
+++ precache-manifest.4191d73c6577a27d71beeb26c6f820b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d39ec22159b3d5536e9625c22453625",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "794c7d34ced7f545e549",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/test/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "794c7d34ced7f545e549",
+    "url": "/test/static/js/main.0358c7f2.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
